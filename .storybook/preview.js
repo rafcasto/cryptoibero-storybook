67b5c96f9f4fe1ae42e82e38aacb897fe3881ebb
opennet <rafcasto@gmail.com>
2021-12-11 12:04:27 +1300

    adding storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import { muiTheme } from 'storybook-addon-material-ui'
+export const decorators = [
+  muiTheme()
+];
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
