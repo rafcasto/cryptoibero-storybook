c9977d6e9d6b5705e6e16fe030dc3e4a48e3b600
opennet <rafcasto@gmail.com>
2021-12-31 15:43:46 +1300

    including npmrc file for packaging

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import {Button} from './components/Button/Button'
 import {Header} from './components/Header/Header'
-export {Button,Header}
+import {CryptoMarketTable} from './components/CryptoMarketTable/CryptoMarketTable'
+import {HeaderTable} from './components/HeaderTable/HeaderTable'
+import {DataTable} from './components/DataTable/DataTable'
+export {
+    Button
+    ,Header
+    ,CryptoMarketTable
+    ,HeaderTable
+    ,DataTable
+}
