63bd885a37ea7782b0ce6d99e1725eca8241cbe8
opennet <rafcasto@gmail.com>
2021-12-31 14:56:43 +1300

    fixing dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,9 +5,7 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "storybook-addon-material-ui"
+    "@storybook/addon-essentials"
   ],
   "framework": "@storybook/react"
 }
