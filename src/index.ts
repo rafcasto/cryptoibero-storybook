1731ee689306c063eecf0ce13369f1c1fcac5800
opennet <rafcasto@gmail.com>
2022-01-06 08:07:31 +1300

    adding cryptochart as component

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import {Header} from './components/Header/Header'
 import {CryptoMarketTable} from './components/CryptoMarketTable/CryptoMarketTable'
 import {HeaderTable} from './components/HeaderTable/HeaderTable'
 import {DataTable} from './components/DataTable/DataTable'
+import {CryptoChart} from './components/CryptoChart/CryptoChart'
 export {
     Button
     ,Header
     ,CryptoMarketTable
     ,HeaderTable
     ,DataTable
+    ,CryptoChart
 }
